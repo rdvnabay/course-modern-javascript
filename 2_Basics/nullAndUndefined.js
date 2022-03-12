02aba80f2e0a1a299c0981b82308fba65fba771c
rdvanabay <ridvanabay@gmail.com>
2022-03-12 22:18:59 +0300

    14. Null & Undefined

---
+++ 2_Basics/nullAndUndefined.js
@@ -0,0 +1,7 @@
+//undefined
+let age
+console.log(age, age + 3, `the age is ${age}`) //undefined NaN 'the age is undefined'
+
+//null
+let age2 = null
+console.log(age2,age2+3,`this age2 is ${age2}`) //null 3 'this age2 is null'
