9f3621cc5a60d47ecbecfd10e87045430db7273d
rdvanabay <ridvanabay@gmail.com>
2022-03-12 20:55:31 +0300

    11. Numbers

---
+++ 2_Basics/numbers.js
@@ -0,0 +1,27 @@
+let radius = 10
+const pi = 3.14
+
+//math operator (+ - * / % **)
+console.log(10 / 2) //5
+
+let result = radius % 4 //1
+result = result ** 2 //2 pow 2 = 4
+
+//order of operation B I D M A S
+result = 5 * (10 - 3) ** 2 //245
+console.log(result)
+
+let likes = 10
+likes = likes + 1 //11
+likes++ //12
+likes-- //11
+likes *= 2 //22
+console.log(likes)
+
+//NaN - not a number 
+console.log('hello' / 5) //NaN
+console.log(5 * 'like') //NaN
+
+console.log('the blog has ' + likes + ' likes') //the blog has 22 likes
+
+
