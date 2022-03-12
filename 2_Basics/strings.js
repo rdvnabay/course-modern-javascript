d3a1ad6f26b5fc6346a34b2ae9d7221bff1e105a
rdvanabay <ridvanabay@gmail.com>
2022-03-12 20:16:36 +0300

    9. Strings

---
+++ 2_Basics/strings.js
@@ -0,0 +1,19 @@
+console.log('Hello')
+console.log("World")
+
+let firstName = 'Rıdvan'
+let lastName = 'Abay'
+
+//string concatenation
+let fullName = firstName + ' ' + lastName
+console.log(fullName) //Rıdvan Abay
+
+//getting character
+console.log(firstName[3]) //v
+
+//length
+console.log(lastName.length) //4
+
+//upper and lower case
+console.log(firstName.toUpperCase()) //RIDVAN
+console.log(lastName.toLowerCase()) //abay
