7b8ead35678ee486c3f52ce2d01f2f56c61f296b
rdvanabay <ridvanabay@gmail.com>
2022-03-12 19:12:43 +0300

    7. Variables, Constants & Comments

---
+++ 2_Basics/variables.js
@@ -0,0 +1,20 @@
+let age = 29
+let year = 2022
+console.log(age, year)
+
+age = 30
+console.log(age)
+
+const pi = 3.14
+console.log(pi)
+
+// pi=3 const variable not change error
+
+//old javascript variable define
+var name = 'Rıdvan'
+console.log(name)
+
+//One line comment
+/* 
+Multiple line comment
+*/
