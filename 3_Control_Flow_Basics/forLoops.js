32770fc1da65daeb0e827a9b306483b65b9dc851
rdvanabay <ridvanabay@gmail.com>
2022-03-13 13:27:05 +0300

    19. For Loops

---
+++ 3_Control_Flow_Basics/forLoops.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+console.log('loop finished')
+
+const names = ['mario', 'shaun', 'luigi']
+
+for (let i = 0; i < names.length; i++) {
+    let html = `<div>${names[i]}</div>`
+    console.log(html)
+}
