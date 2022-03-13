04f6c09eb93126b4c088a9f85842c68d9f5ed4a8
rdvanabay <ridvanabay@gmail.com>
2022-03-13 16:49:44 +0300

    22. If Statements

---
+++ 3_Control_Flow_Basics/ifStatements.js
@@ -0,0 +1,12 @@
+const age = 25
+
+if (age > 20)
+    console.log('you are over 20 years old')
+
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi']
+if (ninjas.length >= 3)
+    console.log("that's a lot of ninjas")
+
+const password = 'pass'
+if (password.length >= 8)
+    console.log('that password is long enough!')
