24f49b9b5340cad600f541514e3fb7b616b715db
rdvanabay <ridvanabay@gmail.com>
2022-03-13 19:13:16 +0300

    30. Function Declarations & Expressions

---
+++ 4_Functions/functionDeclarations.js
@@ -0,0 +1,19 @@
+greet() //hello there
+
+//function declaration
+function greet(){
+    console.log('hello there')
+}
+
+greet() //hello there
+
+
+
+speak() // ERROR: speak is not defined 
+
+//function expression
+const speak=function(){
+    console.log('good day!')
+}
+
+speak() //good day!
