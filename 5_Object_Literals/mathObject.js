e8bd5f5c5057fae430d39c12b787acfeaf68e170
rdvanabay <ridvanabay@gmail.com>
2022-03-14 22:38:03 +0300

    42. Math Object

---
+++ 5_Object_Literals/mathObject.js
@@ -0,0 +1,15 @@
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.E)
+
+const area = 7.7
+
+console.log(Math.round(area)) //8
+console.log(Math.floor(area)) //7
+console.log(Math.ceil(area)) //8
+console.log(Math.trunc(area)) //7
+
+//random numbers
+const random = Math.random()
+console.log(random)
+console.log(Math.round(random * 100))
