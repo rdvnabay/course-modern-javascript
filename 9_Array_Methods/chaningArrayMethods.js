20dda91fba8656c08b345f089a1d775b8550150a
rdvnabay <ridvanabay@gmail.com>
2022-03-23 22:35:15 +0300

    76. Chaining Array Methods

---
+++ 9_Array_Methods/chaningArrayMethods.js
@@ -0,0 +1,23 @@
+const products = [
+    { name: 'gold star', price: 30 },
+    { name: 'green shell', price: 20 },
+    { name: 'red shell', price: 50 },
+    { name: 'mushroom', price: 10 },
+]
+
+const filtered = products.filter(p => p.price > 20)
+const promos = filtered.map(p => {
+    return `the ${p.name} is ${p.price / 2} pounds`
+})
+
+console.log(promos)
+// [ 'the gold star is 15 pounds', 'the red shell is 25 pounds' ]
+
+
+//method chaning 
+const result = products
+    .filter(p => p.price > 20)
+    .map(p => `the ${p.name} is ${p.price / 2} pounds`)
+
+console.log(result)
+// [ 'the gold star is 15 pounds', 'the red shell is 25 pounds' ]
