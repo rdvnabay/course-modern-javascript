17bad797ec96e78fef10edf6a6d36dd354ef23a8
rdvnabay <ridvanabay@gmail.com>
2022-03-31 21:48:27 +0300

    103. Get Weather API Call

--- 13_Project_Weather/scripts/forecast.js
+++ 13_Project_Weather/scripts/forecast.js
@@ -1,15 +1,25 @@
 //AccuWeather api
 const apiKey = 'V4jk2tOAClICDiQxnMYq4XRyQy2SZeqa'
 
+const getWeather = async (locationKey) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${locationKey}?apikey=${apiKey}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+    return data[0]
+}
+
+
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${apiKey}&q=${city}`
 
     const response = await fetch(base + query)
     const data = await response.json()
-    return data
+    return data[0]
 }
 
 getCity('istanbul')
+    .then(data => getWeather(data.Key))
     .then(data => console.log(data))
-    .catch(err => console.log(err))
