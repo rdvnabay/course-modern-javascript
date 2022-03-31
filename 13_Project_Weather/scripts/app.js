355b107ef37352b2afd9eb419c271edae9374c40
rdvnabay <ridvanabay@gmail.com>
2022-03-31 23:00:17 +0300

    108. Weather Icons & images

--- 13_Project_Weather/scripts/app.js
+++ 13_Project_Weather/scripts/app.js
@@ -1,6 +1,8 @@
 const form = document.querySelector('.change-location')
 const card = document.querySelector('.card')
 const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -32,7 +34,20 @@ const updateUI = (data) => {
             <span>${weather.Temperature.Metric.Value}</span>
             <span>&deg;C</span>
         </div>
-`
+    `
+
+    //update the night/day & icon images
+
+    let timeSrc = null
+    if (weather.IsDayTime)
+        timeSrc = 'img/day.svg'
+    else
+        timeSrc = 'img/night.svg'
+
+    time.setAttribute('src',timeSrc)
+
+    const iconSrc=`img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src',iconSrc)
 
     if (card.classList.contains('d-none'))
         card.classList.remove('d-none')
