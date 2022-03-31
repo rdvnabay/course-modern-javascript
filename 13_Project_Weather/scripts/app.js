8e728f0ffedd9b13d6d9f1ece09f017003ea4a63
rdvnabay <ridvanabay@gmail.com>
2022-03-31 22:33:28 +0300

    106. Updating the UI

--- 13_Project_Weather/scripts/app.js
+++ 13_Project_Weather/scripts/app.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('.change-location')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -7,7 +9,7 @@ form.addEventListener('submit', e => {
     form.reset()
 
     //request
-    updateCity(city).then(data => console.log(data))
+    updateCity(city).then(data => updateUI(data))
 
 
 })
@@ -17,4 +19,25 @@ const updateCity = async (city) => {
     const weather = await getWeather(cityDetail.Key)
 
     return { cityDetail, weather }
+}
+
+
+const updateUI = (data) => {
+    const cityDetail = data.cityDetail
+    const weather = data.weather
+
+    console.log('cityDetail', cityDetail)
+    console.log('weather', weather)
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityDetail.LocalizedName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+`
+
+    if (card.classList.contains('d-none'))
+        card.classList.remove('d-none')
 }
