e40024b91da30431c974dd134ba1caa22317ae5c
rdvnabay <ridvanabay@gmail.com>
2022-04-02 14:11:38 +0300

    124. Prototypal Inheritance

--- 15_Object_Oriented/constructorsFunc.js
+++ 15_Object_Oriented/constructorsFunc.js
@@ -3,18 +3,32 @@ function User(name, email) {
     this.email = email
 }
 
-User.prototype.logIn=function(){
+User.prototype.logIn = function () {
     console.log(`${this.name} has logged in`)
     return this
 }
 
-User.prototype.logOut=function(){
+User.prototype.logOut = function () {
     console.log(`${this.name} has logged out`)
     return this
 }
 
+
+function Admin(name, email, age) {
+    User.call(this, name, email) //super()
+    this.age = age
+}
+
+Admin.prototype = Object.create(User.prototype) //inheritance
+
+Admin.prototype.deleteUser = function () {
+    //...
+}
+
+
 const userOne = new User('fikret', 'fikret@mail.com')
 const userTwo = new User('anıl', 'anil@mail.com')
+const userThree = new Admin('zafer', 'zafer@mail.com', 27)
 
-console.log(userOne,userTwo)
+console.log(userOne, userTwo,userThree)
 userOne.logIn().logOut()
