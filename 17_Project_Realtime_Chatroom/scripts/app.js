acb172b393024a46e82f732057c44c8707e6341b
rdvnabay <ridvanabay@gmail.com>
2022-04-06 18:55:38 +0300

    146. Updating the Room

--- 17_Project_Realtime_Chatroom/scripts/app.js
+++ 17_Project_Realtime_Chatroom/scripts/app.js
@@ -2,6 +2,7 @@ const chatList = document.querySelector('.chat-list')
 const newChatForm = document.querySelector('.new-chat')
 const newNameForm = document.querySelector('.new-name')
 const updateMsg = document.querySelector('.update-msg')
+const rooms = document.querySelector('.chat-rooms')
 
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -25,6 +26,14 @@ newNameForm.addEventListener('submit', e => {
     setTimeout(() => {
         updateMsg.innerText = ''
     }, 3000)
+})
+
+rooms.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        chatUI.clear()
+        chatroom.updateRoom(e.target.getAttribute('id'))
+        chatUI.render(data => chatUI.render(data))
+    }
 })
 
 const user = localStorage.user ? localStorage.user : 'rıdvan'
--- 17_Project_Realtime_Chatroom/scripts/ui.js
+++ 17_Project_Realtime_Chatroom/scripts/ui.js
@@ -3,6 +3,10 @@ class ChatUI {
         this.list = list
     }
 
+    clear(){
+        this.list.innerHTML=''
+    }
+
     render(data) {
         const when = dateFns.distanceInWordsToNow(
             data.created_at.toDate(),
