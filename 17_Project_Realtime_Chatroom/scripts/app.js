6f57200423ad9d10a93a855f5df348bd525af46f
rdvnabay <ridvanabay@gmail.com>
2022-04-06 18:15:18 +0300

    144. Sending New Chats

--- 17_Project_Realtime_Chatroom/scripts/app.js
+++ 17_Project_Realtime_Chatroom/scripts/app.js
@@ -1,4 +1,15 @@
 const chatList = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat')
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const message = newChatForm.message.value
+
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err))
+})
 
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom("general", "rıdvan")
